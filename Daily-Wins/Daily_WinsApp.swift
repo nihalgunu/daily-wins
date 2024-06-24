68660552ca6e3c372c90282995a64b38e443111d
Nihal Gunukula <nihalgunukula@gmail.com>
2024-06-23 19:01:13 -0700

    Initial Commit

---
+++ Daily-Wins/Daily_WinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_WinsApp.swift
+//  Daily-Wins
+//
+//  Created by Nihal Gunukula on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Daily_WinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Daily-WinsUITests/Daily_WinsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Daily_WinsUITestsLaunchTests.swift
+//  Daily-WinsUITests
+//
+//  Created by Nihal Gunukula on 6/23/24.
+//
+
+import XCTest
+
+final class Daily_WinsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
