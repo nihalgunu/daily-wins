05dae528e69dce56b083fe330a573667f1012d37
eric-kimm <erickim2208@gmail.com>
2024-07-03 09:26:31 -0700

    added profile button

---
+++ Daily-Wins/ViewModels/TreeViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TreeViewViewModel.swift
+//  Daily-Wins
+//
+//  Created by Eric Kim on 7/3/24.
+//
+
+import Foundation
---
+++ Daily-Wins/Views/TreeView.swift
@@ -0,0 +1,18 @@
+//
+//  TreeView.swift
+//  Daily-Wins
+//
+//  Created by Eric Kim on 7/3/24.
+//
+
+import SwiftUI
+
+struct TreeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TreeView()
+}
